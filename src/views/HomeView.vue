869b2389ecfeb54444f53005d68ed737bbb8264f
hitochix06 <pachara.philibert@hotmail.fr>
2024-03-26 23:31:52 +0100

    Mise à jour de la vue HomeView.vue avec des modifications pour la génération de citations

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,7 +13,6 @@
     <!-- Bar with selection for themes -->
     <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
       <div class="flex flex-col space-y-4">
-        <!-- rajouter  citation -->
         <transition name="slide-fade">
           <div
             v-if="showToast"
@@ -22,6 +21,7 @@
             {{ toastMessage }}
           </div>
         </transition>
+
         <input
           v-model="search"
           class="form-input block w-full text-gray-700 py-3 px-4 border border-gray-300 bg-white rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
@@ -34,10 +34,6 @@
           class="cursor-pointer p-2 rounded hover:bg-[#39C3B4] hover:text-white text-center"
         >
           {{ item.theme }}
-        </div>
-        <!-- Message d'erreur pour le thème non sélectionné -->
-        <div v-if="errorMessage" class="text-red-500 text-center">
-          {{ errorMessage }}
         </div>
         <!-- Le bouton Générer apparaît seulement si un thème est sélectionné -->
         <button
@@ -66,62 +62,56 @@
 </style>
 
 <script setup>
-import { ref, computed, onMounted } from "vue";
+import { ref, computed } from "vue";
 import axios from "axios";
-import { selectData } from "../components/SelectData"; // Importer selectData
+import { selectData } from "../components/SelectData";
 
 const toastMessage = ref("");
 const showToast = ref(false);
-
-// Définir la valeur de isGenerated
-const isGenerated = ref(false);
-
-// Définir la valeur de isLoading
 const isLoading = ref(false);
-
-// Définir la valeur de search
 const search = ref("");
-const selectedTheme = ref(null); // Initialisez selectedTheme avec null pour indiquer qu'aucun thème n'est sélectionné au départ
-const errorMessage = ref(""); // Nouvelle variable pour le message d'erreur
+const selectedTheme = ref(null);
+const errorMessage = ref("");
+const quotes = ref([]); // Assurez-vous d'initialiser quotes si vous souhaitez stocker les citations générées
 
-// Filtrer les données en fonction de la valeur de search
 const filteredData = computed(() => {
-  if (!search.value) return [];
-  return selectData.filter((item) =>
-    item.theme.toLowerCase().startsWith(search.value.toLowerCase())
-  );
+  return search.value
+    ? selectData.filter((item) =>
+        item.theme.toLowerCase().startsWith(search.value.toLowerCase())
+      )
+    : [];
 });
 
 const handleClick = (item) => {
-  selectedTheme.value = item.theme; // Mettez à jour selectedTheme avec le thème sélectionné
-  // Vous pouvez ajouter d'autres actions ici si nécessaire
+  selectedTheme.value = item.theme;
+  errorMessage.value = ""; // Réinitialiser le message d'erreur lorsqu'un thème est sélectionné
 };
 
 const generateQuoteFromAPI = async () => {
+  isLoading.value = true; // Commencer le chargement
   if (!selectedTheme.value) {
     errorMessage.value =
       "Veuillez sélectionner un thème pour générer une citation.";
+    isLoading.value = false;
     return;
   }
   try {
-    // Modifier l'URL et la structure de la requête pour correspondre à la nouvelle API
     const response = await axios.get(
       `https://cita-rapide-backend.vercel.app/v1/quotes/${selectedTheme.value}`
     );
     if (response.data && response.data.length > 0) {
-      const quote = response.data[0]; // Supposer que l'API renvoie un tableau de citations
-      // Envoyer la citation à votre API backend pour stockage, si nécessaire
+      const quote = response.data[0];
       const saveResponse = await axios.post(
         "https://cita-rapide-backend.vercel.app/citations",
         {
-          proverbe: quote.quote, // Assurez-vous que ces champs correspondent à ceux attendus par votre API backend
+          proverbe: quote.quote,
           auteur: quote.author,
           theme: selectedTheme.value,
         }
       );
       if (saveResponse.data && saveResponse.data.result) {
-        // Citation enregistrée avec succès, ajouter à l'état local si nécessaire
         quotes.value.push(saveResponse.data.citation);
+        displayToast("Citation bien rajoutée");
         errorMessage.value = "";
       } else {
         errorMessage.value =
@@ -134,21 +124,16 @@ const generateQuoteFromAPI = async () => {
     console.error(error);
     errorMessage.value =
       "Erreur lors de la connexion à l'API externe ou à l'API backend.";
+  } finally {
+    isLoading.value = false; // Fin du chargement
   }
-  isLoading.value = false; // Fin du chargement
 };
 
-// Modifier la méthode handleGenerate pour utiliser generateQuoteFromAPI
-const handleGenerate = () => {
-  if (!selectedTheme.value) {
-    errorMessage.value =
-      "Veuillez sélectionner un thème avant de générer une citation.";
-    return;
-  }
-  generateQuoteFromAPI();
-  search.value = ""; // Réinitialiser la valeur de search
-  isGenerated.value = true; // Mettre à jour la valeur de isGenerated
-  displayToast("Citation bien rajoutée"); // Afficher un toast pour confirmer l'ajout
+const handleGenerate = async () => {
+  await generateQuoteFromAPI();
+  search.value = ""; // Réinitialiser la recherche
+  selectedTheme.value = null; // Réinitialiser le thème sélectionné
+  displayToast("Citation bien rajoutée");
 };
 
 const displayToast = (message) => {
@@ -156,6 +141,6 @@ const displayToast = (message) => {
   showToast.value = true;
   setTimeout(() => {
     showToast.value = false;
-  }, 3000); // Le toast disparaît après 3 secondes
+  }, 3000);
 };
 </script>
